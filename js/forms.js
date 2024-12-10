9ee1a1ad5ab0ac841e96e13d052676982cf6a8f0
BrenodePaiva <brenopaiva552@gmail.com>
2024-12-10 09:55:12 -0300

    consertando bugs

--- js/forms.js
+++ js/forms.js
@@ -1,97 +1,97 @@
-const form = document.getElementById('form');
-const button = document.getElementById('button');
-const fullname = document.getElementById('name')
-const email = document.getElementById('email')
-const phone = document.getElementById('phone')
-const message = document.getElementById('message')
+const form = document.getElementById("form");
+const button = document.getElementById("button");
+const fullname = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const message = document.getElementById("message");
 
 function checkInputs() {
-   const items = document.querySelectorAll('.item')
+  const items = document.querySelectorAll(".item");
 
-   for (const item of items) {
-      if (item.value == "") {
-         item.classList.add('error')
-         item.parentElement.classList.add('error')
-
-      }
-
-      item.addEventListener('keyup', () => {
-         if (item.value != "") {
-            item.classList.remove('error')
-            item.parentElement.classList.remove('error')
-
-         } else {
-            item.classList.add('error')
-            item.parentElement.classList.add('error')
-         }
-      })
-   }
-}
-
-form.addEventListener('submit', function(e) {
-    const formData = new FormData(form);
-    e.preventDefault();
-    checkInputs()
-
-    const object = Object.fromEntries(formData);
-    const json = JSON.stringify(object);
-
-    button.innerHTML = '<i class="fa fa-refresh fa-spin"></i>'
-
-    if (fullname.classList.contains('error') ||
-    email.classList.contains('error') || phone.classList.contains('error') || 
-    message.classList.contains('error'))  {
-      Swal.fire({
-                  title: 'Error!',
-                  text: 'Preencha todos os campos',
-                  icon: 'error',
-                  confirmButtonText: 'Ok'
-      })
-      return setTimeout(() => {
-         button.innerHTML = 'Enviar'
-      }, 1000)
+  for (const item of items) {
+    if (item.value == "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
     }
 
-    fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: json
-        })
-        .then(async (response) => {
-            if (response.status == 200) {
-               Swal.fire({
-                  title: 'Success!',
-                  text: 'Sua mensagem foi enviada',
-                  icon: 'success',
-                  confirmButtonText: 'Ok'
-                })
+    item.addEventListener("keyup", () => {
+      if (item.value != "") {
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
 
-            } else {
-                Swal.fire({
-                  title: 'Error!',
-                  text: 'Alguma coisa deu errado, tente novamente mais tarde',
-                  icon: 'error',
-                  confirmButtonText: 'Ok'
-                })
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            Swal.fire({
-               title: 'Error!',
-               text: `Alguma coisa deu errado, tente novamente mais tarde. Codigo: ${error}`,
-               icon: 'error',
-               confirmButtonText: 'Ok'
-             })
-        })
-        .then(function() {
-            form.reset();
-            button.innerHTML = 'Enviar'
-            // setTimeout(() => {
-            //     result.style.display = "none";
-            // }, 3000);
+form.addEventListener("submit", function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+  checkInputs();
+
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+
+  button.innerHTML = '<i class="fa fa-refresh fa-spin"></i>';
+
+  if (
+    fullname.classList.contains("error") ||
+    email.classList.contains("error") ||
+    phone.classList.contains("error") ||
+    message.classList.contains("error")
+  ) {
+    Swal.fire({
+      title: "Error!",
+      text: "Preencha todos os campos",
+      icon: "error",
+      confirmButtonText: "Ok",
+    });
+    return setTimeout(() => {
+      button.innerHTML = "Enviar";
+    }, 1000);
+  }
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      if (response.status == 200) {
+        Swal.fire({
+          title: "Success!",
+          text: "Sua mensagem foi enviada",
+          icon: "success",
+          confirmButtonText: "Ok",
         });
+      } else {
+        Swal.fire({
+          title: "Error!",
+          text: "Alguma coisa deu errado, tente novamente mais tarde",
+          icon: "error",
+          confirmButtonText: "Ok",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      Swal.fire({
+        title: "Error!",
+        text: `Alguma coisa deu errado, tente novamente mais tarde. Codigo: ${error}`,
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
+    })
+    .then(function () {
+      form.reset();
+      button.innerHTML = "Enviar";
+      // setTimeout(() => {
+      //     result.style.display = "none";
+      // }, 3000);
+    });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,124 @@
 // quando o scroll smooth no css não funciona (Tirar depois)
-const menuLinks = document.querySelectorAll('header nav a[href ^= "/#"]')
+// const menuLinks = document.querySelectorAll('header nav a[href ^= "/#"]')
 
-function getDistanceFromTheTop(element) {
-    const id = element.getAttribute('href')
-    const idStrin = id.split('/')
-    return document.querySelector(idStrin[1]).offsetTop
-}
+// function getDistanceFromTheTop(element) {
+//     const id = element.getAttribute('href')
+//     const idStrin = id.split('/')
+//     return document.querySelector(idStrin[1]).offsetTop
+// }
 
-function scrollToSection(event) {
-    event.preventDefault()
-    const distanceFromTheTop = getDistanceFromTheTop(event.target) - 60
-    smoothScrollTo(0, distanceFromTheTop)
-}
+// function scrollToSection(event) {
+//     event.preventDefault()
+//     const distanceFromTheTop = getDistanceFromTheTop(event.target) - 60
+//     smoothScrollTo(0, distanceFromTheTop)
+// }
 
-menuLinks.forEach(link => {
-    link.addEventListener('click', scrollToSection)
-});
+// menuLinks.forEach(link => {
+//     link.addEventListener('click', scrollToSection)
+// });
 
-function smoothScrollTo(endX, endY, duration) {
-    const startX = window.scrollX || window.pageXOffset;
-    const startY = window.scrollY || window.pageYOffset;
-    const distanceX = endX - startX;
-    const distanceY = endY - startY;
-    const startTime = new Date().getTime();
-  
-    duration = typeof duration !== "undefined" ? duration : 400;
-  
-    const easeInOutQuart = (time, from, distance, duration) => {
-      if ((time /= duration / 2) < 1)
-        return (distance / 2) * time * time * time * time + from;
-      return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
-    };
-  
-    const timer = setInterval(() => {
-      const time = new Date().getTime() - startTime;
-      const newX = easeInOutQuart(time, startX, distanceX, duration);
-      const newY = easeInOutQuart(time, startY, distanceY, duration);
-      if (time >= duration) {
-        clearInterval(timer);
-      }
-      window.scroll(newX, newY);
-    }, 1000 / 60);
-  }
+// function smoothScrollTo(endX, endY, duration) {
+//     const startX = window.scrollX || window.pageXOffset;
+//     const startY = window.scrollY || window.pageYOffset;
+//     const distanceX = endX - startX;
+//     const distanceY = endY - startY;
+//     const startTime = new Date().getTime();
+
+//     duration = typeof duration !== "undefined" ? duration : 400;
+
+//     const easeInOutQuart = (time, from, distance, duration) => {
+//       if ((time /= duration / 2) < 1)
+//         return (distance / 2) * time * time * time * time + from;
+//       return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+//     };
+
+//     const timer = setInterval(() => {
+//       const time = new Date().getTime() - startTime;
+//       const newX = easeInOutQuart(time, startX, distanceX, duration);
+//       const newY = easeInOutQuart(time, startY, distanceY, duration);
+//       if (time >= duration) {
+//         clearInterval(timer);
+//       }
+//       window.scroll(newX, newY);
+//     }, 1000 / 60);
+//   }
 // ------------------------------------------
 
-window.onscroll = () => {
-const changeBackgroud = document.querySelector('header')
-const sections = document.querySelectorAll('section')
-const navLinks = document.querySelectorAll('header nav a')
-const linskBack = document.querySelector('.link-back')
+const linskBack = document.querySelector(".link-back");
+const screen = {
+  large: null,
+  medium: window.matchMedia("(max-width: 960px)"),
+};
 
-    if (window.pageYOffset > 150) {
-        changeBackgroud.classList.add('blur')
-        linskBack.style.opacity = 1
-    }
-    if (window.pageYOffset <= 150) {
-        changeBackgroud.classList.remove('blur')
-        linskBack.style.opacity = 0
-    }
-
-    sections.forEach(sec => {
-        const top = window.scrollY
-        const offset = sec.offsetTop - 100
-        const height = sec.offsetHeight
-        const id = sec.getAttribute('id')
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active')
-                document
-                .querySelector('header nav a[href*=' + id + ']')
-                .classList.add('active')
-          })
-        }
-    })
+for (const [screenSize, mediaQuery] of Object.entries(screen)) {
+  if (mediaQuery) mediaQuery.addEventListener("change", mqHandler);
 }
 
-const toggleOpen = document.querySelector('.bi.bi-list.to')
-const nav = document.querySelector('nav')
-const toggleClose = document.querySelector('.bi.bi-x-square')
+function mqHandler() {
+  for (const [screenSize, mediaQuery] of Object.entries(screen)) {
+    if (!mediaQuery || mediaQuery.matches) {
+      if (screenSize == "medium") {
+        linskBack.style.opacity = 1;
+      } else if (screenSize == "large") {
+        linskBack.style.opacity = 0;
+      }
+    }
+  }
+}
+
+mqHandler();
+
+window.onscroll = () => {
+  const changeBackgroud = document.querySelector("header");
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("header nav a");
+
+  if (window.pageYOffset > 150) {
+    changeBackgroud.classList.add("blur");
+    linskBack.style.opacity = 1;
+  }
+  if (window.pageYOffset <= 150) {
+    changeBackgroud.classList.remove("blur");
+    linskBack.style.opacity = 0;
+  }
+
+  sections.forEach((sec) => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 100;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+const toggleOpen = document.querySelector(".bi.bi-list.to");
+const nav = document.querySelector("nav");
+const toggleClose = document.querySelector(".bi.bi-x-square");
 
 function navToggleOpen() {
-    toggleOpen.classList.add('toggle')
-    toggleClose.classList.add('nav-toggle-close')
-    nav.classList.add('nav-open')
-    
+  toggleOpen.classList.add("toggle");
+  toggleClose.classList.add("nav-toggle-close");
+  nav.classList.add("nav-open");
 }
-
 
 function navToggleClose() {
-    toggleOpen.classList.remove('toggle')
-    toggleClose.classList.remove('nav-toggle-close')
-    nav.classList.remove('nav-open')
+  toggleOpen.classList.remove("toggle");
+  toggleClose.classList.remove("nav-toggle-close");
+  nav.classList.remove("nav-open");
 }
 
-toggleOpen.addEventListener('click', navToggleOpen)
-toggleClose.addEventListener('click', navToggleClose)
+toggleOpen.addEventListener("click", navToggleOpen);
+toggleClose.addEventListener("click", navToggleClose);
 
-
-
-const date = new Date()
-const currentYear = date.getFullYear()
-document.getElementById('current-year').innerHTML = '&nbsp;' + currentYear + '&nbsp;'
+const date = new Date();
+const currentYear = date.getFullYear();
+document.getElementById("current-year").innerHTML =
+  "&nbsp;" + currentYear + "&nbsp;";
